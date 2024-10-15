2867a9f2053abe833f77e196f1d26c331514c819
unknown <nikolinarogan67@gmail.com>
2024-10-15 20:53:07 +0200

    Updated service and controller

--- src/Modules/Tours/Explorer.Tours.API/Public/Administration/IObjectService.cs
+++ src/Modules/Tours/Explorer.Tours.API/Public/Administration/IObjectService.cs
@@ -1,4 +1,5 @@
-﻿using Explorer.Tours.API.Dtos;
+﻿using Explorer.BuildingBlocks.Core.UseCases;
+using Explorer.Tours.API.Dtos;
 using FluentResults;
 using System;
 using System.Collections.Generic;
@@ -11,5 +12,6 @@ namespace Explorer.Tours.API.Public.Administration
     public interface IObjectService
     {
         Result<ObjectDto> Create(ObjectDto tourObject);
+        PagedResult<ObjectDto> GetAll();
     }
 }
