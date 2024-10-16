03c7e77204bb7aa50f3af11dc0a4990471a9d16f
srdjan <srdjanilic.002@gmail.com>
2024-10-16 02:57:53 +0200

    feat:accountController put request fix

--- src/Modules/Stakeholders/Explorer.Stakeholders.Core/Mappers/StakeholderProfile.cs
+++ src/Modules/Stakeholders/Explorer.Stakeholders.Core/Mappers/StakeholderProfile.cs
@@ -24,6 +24,7 @@ public class StakeholderProfile : Profile
             .ForMember(dest => dest.MimeType, opt => opt.MapFrom(src => src.GetMimeTypeNormalized));
 
         CreateMap<Person, AccountDto>()
+            .ForMember(dest => dest.UserId, opt => opt.MapFrom(src => src.UserId))
             .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.User.Username))
             .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email))
             .ForMember(dest => dest.Role, opt => opt.MapFrom(src => (int)src.User.Role))
