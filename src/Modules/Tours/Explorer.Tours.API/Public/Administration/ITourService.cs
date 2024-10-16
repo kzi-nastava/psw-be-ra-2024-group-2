a49a15ee8e97276b065e041f349e146c13254b2f
galijander <galicstrahinja@gmail.com>
2024-10-16 15:53:20 +0200

    fix/adjusting review controler

--- src/Modules/Tours/Explorer.Tours.API/Public/Administration/ITourService.cs
+++ src/Modules/Tours/Explorer.Tours.API/Public/Administration/ITourService.cs
@@ -14,6 +14,8 @@ public interface ITourService
         Result<TourDto> UpdateTour(TourDto tourDto, long userId);
         Result<TourDto> CreateTour(TourDto dto, int userId);
         PagedResult<TourDto> GetAllByUserId(int userId);
+        Result<PagedResult<TourDto>> GetPaged(int page, int pageSize);
 
-    
+
+
 }
--- src/Modules/Tours/Explorer.Tours.Tests/TestData/e-tourReview.sql
+++ src/Modules/Tours/Explorer.Tours.Tests/TestData/e-tourReview.sql
@@ -8,4 +8,4 @@ INSERT INTO tours."TourReview"(
 
 INSERT INTO tours."TourReview"(
 	"Id", "Grade", "Comment", "UserId", "TourId", "ImageId", "ReviewDate", "VisitDate")
-	VALUES (-3, 5, 'Amazing to!', -2, -15, NULL, '2024-10-14', '2024-09-20');
+	VALUES (-3, 5, 'Amazing to!', -2, -10, NULL, '2024-10-14', '2024-09-20');
