cef0f574670eec9722f0fec7f38ebef1090e9d94
AleksandarCvijetic <acacvijetic02@gmail.com>
2024-10-27 14:25:03 +0100

    Tourist position model created

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/TouristPosition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public class TouristPosition
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+    }
+}
