0f8895567a29b7ae4b3a686b0068f0594b122661
Stefan Trkulja <trkulja.stefander33@gmail.com>
2024-10-29 21:09:36 +0100

    Added tour execution model and service/tour execution checkpoint/ aggregate

---
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/TourExecutionCheckpoint.cs
@@ -0,0 +1,32 @@
+﻿using Explorer.BuildingBlocks.Core.Domain;
+using Explorer.BuildingBlocks.Core.Domain.Enums;
+using Explorer.Tours.Core.Domain.RepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Explorer.Tours.Core.Domain
+{
+    public class TourExecutionCheckpoint : Entity
+    {
+        public long TourExecutionId {  get; private set; }
+        public long CheckpointId {  get; private set; }
+        public Checkpoint Checkpoint { get; private set; }
+        public CheckpointStatus Status { get; private set; }
+        public DateTime ArrivalAt {  get; private set; }
+
+
+        public TourExecutionCheckpoint(long tourExecutionId, CheckpointStatus status, DateTime arrivalAt, long checkpointId)
+        {
+            TourExecutionId = tourExecutionId;
+            Status = status;
+            ArrivalAt = arrivalAt;
+            CheckpointId = checkpointId;
+        }
+
+
+
+    }
+}
--- src/Modules/Tours/Explorer.Tours.Infrastructure/Database/ToursContext.cs
+++ src/Modules/Tours/Explorer.Tours.Infrastructure/Database/ToursContext.cs
@@ -1,6 +1,7 @@
 ﻿using Explorer.BuildingBlocks.Core.Domain;
 using Explorer.Stakeholders.Core.Domain;
 using Explorer.Tours.Core.Domain;
+using Explorer.Tours.Core.Domain.RepositoryInterfaces;
 using Microsoft.EntityFrameworkCore;
 
 namespace Explorer.Tours.Infrastructure.Database;
@@ -19,18 +20,23 @@ public class ToursContext : DbContext
     public DbSet<TourPreference> TourPreferences { get; set; }
     public DbSet<TourPreferenceTag> PreferenceTags { get; set; }
     public DbSet<TourDurationByTransport> TourDurationByTransports { get; set; }
+
+    public DbSet<TourExecution> TourExecutions { get; set; }
+
+    public DbSet<TourExecutionCheckpoint> TourExecutionCheckpoints { get; set; }
+
     public ToursContext(DbContextOptions<ToursContext> options) : base(options) {}
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema("tours");
         modelBuilder.Entity<Club>()
-        .Property(c => c.ImageId)
-        .IsRequired(false);
+            .Property(c => c.ImageId)
+            .IsRequired(false);
 
         modelBuilder.Entity<Club>()
-        .Property(c => c.OwnerId)
-        .IsRequired();
+            .Property(c => c.OwnerId)
+            .IsRequired();
 
         modelBuilder.Entity<TourReview>()
             .HasOne(p => p.Image)
@@ -44,6 +50,33 @@ public class ToursContext : DbContext
             .HasForeignKey(t => t.TourId)
             .OnDelete(DeleteBehavior.Cascade);
 
-       
+        //modelBuilder.Entity<TourExecution>()
+        //    .HasOne(te => te.Tour)
+        //    .WithMany()
+        //    .HasForeignKey(te => te.TourId)
+        //    .OnDelete(DeleteBehavior.Cascade);
+
+        //modelBuilder.Entity<TourExecution>()
+        //    .HasMany(te => te.tourExecutionCheckpoints)
+        //    .WithOne(tc => tc.TourExecution)
+        //    .HasForeignKey(tc => tc.TourExecutionId)  // Ensure correct FK mapping
+        //    .OnDelete(DeleteBehavior.Cascade);
+
+        //modelBuilder.Entity<TourExecutionCheckpoint>()
+        //    .HasOne(tc => tc.Checkpoint)
+        //    .WithMany()
+        //    .HasForeignKey(tc => tc.CheckpointId)  // Use correct FK for Checkpoint
+        //    .OnDelete(DeleteBehavior.Restrict);
+
+
+        modelBuilder.Entity<TourExecution>()
+            .HasMany(te => te.tourExecutionCheckpoints)
+            .WithOne()
+            .OnDelete(DeleteBehavior.Cascade);
+        //modelBuilder.Entity<TourExecutionCheckpoint>()
+        //    .HasOne(kp => kp.Checkpoint)
+        //    .WithOne()
+        //    .OnDelete(DeleteBehavior.Cascade);
+
     }
 }
