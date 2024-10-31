43d1e009f2c45016cdd3469a87436aedaad307fa
Nenad Gvozdenac <nenadgvozdenacsrb@gmail.com>
2024-10-31 17:42:47 +0100

    added example of ValueObject: DurationByTourTransport

--- src/Modules/Tours/Explorer.Tours.Core/Domain/TourDurationByTransport.cs
+++ src/Modules/Tours/Explorer.Tours.Core/Domain/TourDurationByTransport.cs
@@ -1,25 +1,24 @@
 ﻿using Explorer.BuildingBlocks.Core.Domain;
 using Explorer.BuildingBlocks.Core.Domain.Enums;
+using System.Text.Json.Serialization;
 
 namespace Explorer.Tours.Core.Domain;
 
-public class TourDurationByTransport : Entity
+public class TourDurationByTransport : ValueObject
 {
     public TransportType TransportType { get; private set; }
     public double Duration { get; private set; }
-    public long TourId { get; private set; }
-    public Tour Tour { get; private set; }
-    public TourDurationByTransport(long tourId, TransportType transportType, double duration)
+
+    [JsonConstructor]
+    public TourDurationByTransport(TransportType transportType, double duration)
     {
-        TourId = tourId;
         TransportType = transportType;
         Duration = duration;
     }
 
-    public TourDurationByTransport(long tourId, string transportType, double duration)
+    protected override IEnumerable<object> GetEqualityComponents()
     {
-        TourId = tourId;
-        TransportType = Enum.Parse<TransportType>(transportType);
-        Duration = duration;
+        yield return TransportType;
+        yield return Duration;
     }
 }
