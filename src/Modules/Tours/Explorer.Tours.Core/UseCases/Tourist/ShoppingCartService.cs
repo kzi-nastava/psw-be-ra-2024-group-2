ba240388f35ef877fc67af58e23d3c8d56fcc9d7
Ana Mićić <148842149+aanmii@users.noreply.github.com>
2024-11-07 05:00:00 -0800

    Aggregate implemented, http methods fixed

--- src/Modules/Tours/Explorer.Tours.Core/UseCases/Tourist/ShoppingCartService.cs
+++ src/Modules/Tours/Explorer.Tours.Core/UseCases/Tourist/ShoppingCartService.cs
@@ -1,12 +1,9 @@
 ﻿using AutoMapper;
-using Explorer.Tours.API.Public.Tourist;
-using Explorer.Tours.API.Public.Tourist.DTOs;
-using Explorer.Tours.Core.Domain;
-using System.Collections.Generic;
-using FluentResults;
 using Explorer.BuildingBlocks.Core.UseCases;
-using Explorer.Tours.Core.Domain.RepositoryInterfaces;
-using Microsoft.EntityFrameworkCore.Query.Internal;
+using Explorer.Tours.API.Public.Tourist.DTOs;
+using Explorer.Tours.API.Public.Tourist;
+using Explorer.Tours.Core.Domain;
+using FluentResults;
 
 public class ShoppingCartService : IShoppingCartService
 {
@@ -15,7 +12,11 @@ public class ShoppingCartService : IShoppingCartService
     private readonly ICrudRepository<OrderItem> _orderItemRepository;
     private readonly IMapper _mapper;
 
-    public ShoppingCartService(IShoppingCartRepository shoppingCartRepository, ICrudRepository<Tour> tourRepository, ICrudRepository<OrderItem> orderItemRepository, IMapper mapper)
+    public ShoppingCartService(
+        IShoppingCartRepository shoppingCartRepository,
+        ICrudRepository<Tour> tourRepository,
+        ICrudRepository<OrderItem> orderItemRepository, 
+        IMapper mapper)
     {
         _shoppingCartRepository = shoppingCartRepository;
         _tourRepository = tourRepository;
@@ -25,58 +26,79 @@ public class ShoppingCartService : IShoppingCartService
 
     public Result AddItemToCart(long userId, OrderItemDto orderItemDto)
     {
-        var allTours = _tourRepository.GetPaged(1, int.MaxValue);
-        var tour = allTours.Results.FirstOrDefault(t => t.Id == orderItemDto.TourId);
+        var tour = _tourRepository.GetPaged(1, int.MaxValue).Results
+                      .FirstOrDefault(t => t.Id == orderItemDto.TourId);
         if (tour == null)
         {
             return Result.Fail("Item not found.");
         }
 
         var orderItem = _mapper.Map<OrderItem>(orderItemDto);
+        orderItem.Price = tour.Price;
+        orderItem.TourName = tour.Name;
 
         var cart = _shoppingCartRepository.GetByUserId(userId);
         if (cart == null)
         {
-            cart = new ShoppingCart();
+            cart = new ShoppingCart(userId);
             _shoppingCartRepository.Create(cart);
-
+            cart.AddItem(orderItem);
+            _shoppingCartRepository.Update(cart);
         }
-        
-        orderItem.TourName = tour.Name;
-        orderItem.Price = tour.Price;
-        orderItem.UserId = userId;
-        cart.AddItem(orderItem);
-        _shoppingCartRepository.Update(cart);
+        else
+        {
+            cart.AddItem(orderItem);
+            _shoppingCartRepository.Update(cart);
+        }
+       
 
         return Result.Ok();
     }
-
-
+    
     public Result RemoveItemFromCart(long userId, OrderItemDto orderItemDto)
     {
         var cart = _shoppingCartRepository.GetByUserId(userId);
         var orderItem = _mapper.Map<OrderItem>(orderItemDto);
-        //cart.RemoveItem(orderItem);
-        //_shoppingCartRepository.Update(cart);#
-        var allItems = _orderItemRepository.GetPaged(1, int.MaxValue);
-        var order = allItems.Results.FirstOrDefault(o => o.TourId == orderItemDto.TourId && o.ShoppingCartId == cart.Id);
-        _orderItemRepository.Delete(order.Id);
-        return Result.Ok(); 
-    }
 
+        cart.RemoveItem(orderItem);
+        _shoppingCartRepository.Update(cart);
+
+        return Result.Ok();
+    }
+    
+        public Result Checkout(long userId)
+        {
+
+            var cart = _shoppingCartRepository.GetByUserId(userId);
+
+
+            var orderItems = _orderItemRepository.GetPaged(1, int.MaxValue).Results
+                .Where(item => item.UserId == cart.TouristId)
+                .ToList();
+
+
+            foreach (var item in orderItems)
+            {
+                item.Token = true;
+                _orderItemRepository.Update(item); 
+            }
+
+            return Result.Ok();
+        }
+    
+/*
     public Result Checkout(long userId)
     {
         var cart = _shoppingCartRepository.GetByUserId(userId);
         cart.Checkout();
         _shoppingCartRepository.Update(cart);
+
         return Result.Ok();
     }
-
+*/
     public double GetTotalPrice(long userId)
     {
         var cart = _shoppingCartRepository.GetByUserId(userId);
         return cart.GetTotalPrice();
     }
-
-  
 }
