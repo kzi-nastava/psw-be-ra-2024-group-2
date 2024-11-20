5353672e40c7e8b1d662a2df117acfc2f619df57
Stefan Trkulja <trkulja.stefander33@gmail.com>
2024-11-20 03:12:56 +0100

    Added event acception

--- src/Modules/Tours/Explorer.Tours.API/Public/Administration/IEventService.cs
+++ src/Modules/Tours/Explorer.Tours.API/Public/Administration/IEventService.cs
@@ -11,7 +11,9 @@ namespace Explorer.Tours.API.Public.Administration
 {
     public interface IEventService
     {
+        PagedResult<EventDto> GetAllEventsWithinRange(int v, double longitude, double latitude);
         Result<EventDto> Create(EventDto tourEvent);
         PagedResult<EventDto> GetAll();
+        Result SubscribeToEvent(EventDto eventDto, int userId);
     }
 }
