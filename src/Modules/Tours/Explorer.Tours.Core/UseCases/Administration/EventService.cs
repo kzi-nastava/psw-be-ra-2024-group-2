7b80210ebdce35b0580d0310bc8e3647d2354396
unknown <nikolinarogan67@gmail.com>
2024-11-25 04:54:46 +0100

    Feat: event analytics - Author can see events sorted by popularity - Tourist can see top 3 popular events - Tourist can see tours that are near by event he selected

--- src/Modules/Tours/Explorer.Tours.Core/UseCases/Administration/EventService.cs
+++ src/Modules/Tours/Explorer.Tours.Core/UseCases/Administration/EventService.cs
@@ -5,6 +5,7 @@ using Explorer.BuildingBlocks.Core.UseCases;
 using Explorer.Tours.API.Dtos;
 using Explorer.Tours.API.Public.Administration;
 using Explorer.Tours.Core.Domain;
+using Explorer.Tours.Core.Domain.RepositoryInterfaces;
 using FluentResults;
 using System;
 using System.Collections;
@@ -19,10 +20,14 @@ namespace Explorer.Tours.Core.UseCases.Administration
     {
         private readonly ICrudRepository<Event> _eventRepository;
         private readonly IImageRepository _imageRepository;
-        public EventService(ICrudRepository<Event> repository, IImageRepository imageRepository, IMapper mapper) : base(repository, mapper)
+        private readonly ICrudRepository<Tour> _tourRepository;
+        private readonly IMapper _mapper;
+        public EventService(ICrudRepository<Event> repository, IImageRepository imageRepository, ICrudRepository<Tour> tourRepository, IMapper mapper) : base(repository, mapper)
         {
             _eventRepository = repository;
             _imageRepository = imageRepository;
+            _tourRepository = tourRepository;
+            _mapper = mapper;
         }
         public PagedResult<EventDto> GetAll()
         {
@@ -98,6 +103,81 @@ namespace Explorer.Tours.Core.UseCases.Administration
             _eventRepository.Update(e);
             return Result.Ok();
             
+        }
+
+        public PagedResult<EventDto> GetSortedByAcceptance()
+        {
+
+            var allEvents = _eventRepository.GetPaged(1, int.MaxValue);
+            var sortedEvents = allEvents.Results
+               .OrderByDescending(e => e.EventAcceptances.Count)
+               .ToList();
+
+            var events = new List<EventDto>();
+            foreach(Event e in sortedEvents)
+            {
+                events.Add(MapToDto(e));
+            }
+
+
+            return new PagedResult<EventDto>(events, events.Count());
+        }
+        public PagedResult<EventDto> GetTopThree()
+        {
+
+            var allEvents = _eventRepository.GetPaged(1, int.MaxValue);
+            var sortedEvents = allEvents.Results
+               .OrderByDescending(e => e.EventAcceptances.Count)
+               .ToList();
+
+            var events = new List<EventDto>();
+            int counter = 0;
+            foreach (Event e in sortedEvents)
+            {
+                if(counter < 3)
+                {
+                    events.Add(MapToDto(e));
+                    counter++;
+                }
+            }
+
+            return new PagedResult<EventDto>(events, events.Count());
+        }
+        public PagedResult<TourDto> GetNearTours(long eventId)
+        {
+            var tours = _tourRepository.GetPaged(1, int.MaxValue).Results;
+            var eventt = _eventRepository.Get(eventId);
+
+            var nearbyTours = tours.Where(tour =>
+            {
+                var firstCheckpoint = tour.Checkpoints.FirstOrDefault();
+                if (firstCheckpoint == null)
+                {
+                    return false;
+                }
+
+                double distance = CalculateDistance(eventt.Latitude, eventt.Longitude,
+                                                    firstCheckpoint.Latitude, firstCheckpoint.Longitude);
+                return distance <= 30000;
+            }).ToList();
+            var dtos = nearbyTours.Select(t => _mapper.Map<TourDto>(t)).ToList();
+            return new PagedResult<TourDto>(dtos.ToList(), nearbyTours.Count());
+        }
+        private double CalculateDistance(double lat1, double lon1, double lat2, double lon2)
+        {
+            const double R = 6371;
+
+            double latRad1 = lat1 * (Math.PI / 180);
+            double latRad2 = lat2 * (Math.PI / 180);
+            double deltaLat = (lat2 - lat1) * (Math.PI / 180);
+            double deltaLon = (lon2 - lon1) * (Math.PI / 180);
+
+            double a = Math.Sin(deltaLat / 2) * Math.Sin(deltaLat / 2) +
+                       Math.Cos(latRad1) * Math.Cos(latRad2) *
+                       Math.Sin(deltaLon / 2) * Math.Sin(deltaLon / 2);
+            double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+            return R * c;
         }
     }
 }
